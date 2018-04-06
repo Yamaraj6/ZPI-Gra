49a3bb4aee771df50a13bb44d39eb5b486ad825e
Kori08 <Kori08@users.noreply.github.com>
2018-04-06 13:45:10 +0200

    Fixy osi i kamery

--- Assets/Player/CameraMove.cs
+++ Assets/Player/CameraMove.cs
@@ -4,12 +4,13 @@ public class CameraMove : MonoBehaviour {
 
     [SerializeField]
     GameObject player;
+    public Vector3 cameraOffset;
+    public float smoothSpeed = 0.125f;
 	
-	void Update ()
+	void LateUpdate ()
     {
-        gameObject.transform.position = new Vector3(
-            gameObject.transform.position.x,
-            gameObject.transform.position.y, 
-            player.transform.position.z+2);
+        Vector3 targetPosition = player.transform.position + cameraOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
+        gameObject.transform.position = smoothedPosition;
     }
 }
