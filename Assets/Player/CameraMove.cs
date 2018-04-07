f43cc5facb4a4f0b1631b683b1037386018e1883
Kori08 <Kori08@users.noreply.github.com>
2018-04-08 01:46:52 +0200

    Ruch kamery w trybach: standardowy i boss area

--- Assets/Player/CameraMove.cs
+++ Assets/Player/CameraMove.cs
@@ -6,11 +6,37 @@ public class CameraMove : MonoBehaviour {
     GameObject player;
     public Vector3 cameraOffset;
     public float smoothSpeed = 0.125f;
-	
-	void LateUpdate ()
+
+    public bool bossLevel;
+    [HideInInspector]
+    public GameObject boss;
+    [HideInInspector]
+    public Vector3 cameraOffset_boss_fromPlayer;
+    [HideInInspector]
+    public Vector3 cameraOffset_boss_fromBoss;
+
+
+    void LateUpdate()
+    {
+        if (!bossLevel)
+            MoveClassicCamera();
+        else
+            MoveBossCamera();
+    }
+
+    void MoveClassicCamera()
     {
         Vector3 targetPosition = player.transform.position + cameraOffset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
         gameObject.transform.position = smoothedPosition;
+    }
+
+    void MoveBossCamera()
+    {        
+        Vector3 targetPosition = player.transform.position + cameraOffset_boss_fromPlayer;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
+        gameObject.transform.position = smoothedPosition;
+        
+        transform.forward = boss.transform.position + cameraOffset_boss_fromBoss - transform.position;
     }
 }
