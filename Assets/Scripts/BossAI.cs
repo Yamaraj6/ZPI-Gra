a860382e9b610a05e34c512462fbba2d0a51bb2a
Kori08 <Kori08@users.noreply.github.com>
2018-04-09 09:15:26 +0200

    2 Camera Modes + Boss + BossAreaEntry + Level + MovingPlatform

---
+++ Assets/Scripts/BossAI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        LookAtPlayer();
+	}
+
+    void LookAtPlayer()
+    {
+        Vector3 _target = GameObject.FindGameObjectWithTag("Player").transform.position;
+        _target.y = transform.position.y;
+        transform.LookAt(_target);
+    }
+}
---
+++ Assets/Scripts/HPManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPManager : MonoBehaviour {
+
+    public int maxHP = 100;
+    private int actualHP = 100;
+
+    [Tooltip("Dodaj jeśli potrzebujesz wyświetlać liczbę HP")]
+    public TextMesh text;
+    
+	void Start () {
+        actualHP = maxHP;
+	}
+
+    public void ReduceHP(int hp_points)
+    {
+        actualHP -= hp_points;
+        AfterReduce();
+    }
+
+    public void ReduceHP(float hp_percentage)
+    {
+        actualHP -= (int) (maxHP * hp_percentage);
+        AfterReduce();
+    }
+
+    private void AfterReduce()
+    {
+        if (actualHP <= 0)
+        {
+            Destroy(gameObject);
+        } else if (text != null)
+        {
+            text.text = "" + actualHP;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log(gameObject.name + "Destroyed");
+    }
+
+    /*
+    Do spella dodać event na zderzeniu z bossem:
+ 
+     if (collider.gameObject.tag == "Boss")
+        {
+            collider.gameObject.GetComponent<HPManager>().ReduceHP(damage);
+        }
+
+     */
+}
