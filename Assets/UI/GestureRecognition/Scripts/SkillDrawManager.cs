b769aae5cd00140d8c0e1db78ae27b1467045627
hkoscielski <huberto1010@op.pl>
2018-04-13 16:30:22 +0200

    Rysowanie czarow za pomoca particli

--- Assets/UI/GestureRecognition/Scripts/SkillDrawManager.cs
+++ Assets/UI/GestureRecognition/Scripts/SkillDrawManager.cs
@@ -1,33 +1,26 @@
 ﻿using PDollarGestureRecognizer;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class SkillDrawManager : MonoBehaviour
-{
+{    
     [SerializeField]
-    private Transform gestureOnScreenPrefab;
+    private ParticleSystem particle;
 
+    private Rect drawArea;    
     private RuntimePlatform platform;
-
-    private Vector3 virtualKeyPosition = Vector2.zero;
-    private List<LineRenderer> gestureLinesRenderer = new List<LineRenderer>();
-    private LineRenderer currentGestureLineRenderer;
-    private Rect drawArea;
-    private int vertexCount = 0;
-
     private List<Point> points = new List<Point>();
-    private int strokeId = -1;
-
     private bool recognized;
+    private int strokeId = -1;
+    private int vertexCount = 0;
+    private Vector3 virtualKeyPosition;
 
     void Start()
     {
         platform = Application.platform;
-        RectTransform rt = this.GetComponent<RectTransform>();
-
-        drawArea = ConvertToScreenSize(rt);
+        RectTransform _rt = this.GetComponent<RectTransform>();                
+        drawArea = ConvertToScreenSize(_rt);
+        particle.gameObject.SetActive(false);
         Debug.Log(string.Format("DrawArea Position: ({0}, {1})", drawArea.position.x, drawArea.position.y));
         Debug.Log(string.Format("DrawArea Size: ({0}, {1})", drawArea.size.x, drawArea.size.y));
     }
@@ -35,83 +28,53 @@ public class SkillDrawManager : MonoBehaviour
     void Update()
     {
         virtualKeyPosition = GetTouchPosition();
-
+        
         if (drawArea.Contains(virtualKeyPosition))
-        {
+        {            
+            particle.transform.position = virtualKeyPosition;            
+            particle.gameObject.SetActive(true);
             if (Input.GetMouseButtonDown(0))
             {
                 if (recognized)
                 {
                     ClearDrawPanel();
                 }
-                ++strokeId;
-
-                InstantiateNewLine();
+                ++strokeId;                
                 vertexCount = 0;
-            }
-
+            }           
+            
             if (Input.GetMouseButton(0))
             {
                 AddDrawnPoints();
             }
         }
         if (Input.GetMouseButtonUp(0))
-        {
+        {                     
             FinishDraw();            
         }        
-    }
+    }    
 
-    private Vector3 GetTouchPosition()
+    private void AddDrawnPoints()
     {
-        Vector3 virtualKeyPosition = Vector3.zero;
-
-        if (platform == RuntimePlatform.Android)
-        {
-            if (Input.touchCount > 0)
-            {
-                virtualKeyPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);                
-            }
-        }
-        else
-        {
-            if (Input.GetMouseButton(0))
-            {
-                virtualKeyPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);                
-            }
-        }
-
-        return virtualKeyPosition;
+        points.Add(new Point(virtualKeyPosition.x, -virtualKeyPosition.y, strokeId));        
     }
 
     private void ClearDrawPanel()
     {
         recognized = false;
         strokeId = -1;
-
         points.Clear();
 
-        foreach (LineRenderer lineRenderer in gestureLinesRenderer)
-        {
-            lineRenderer.positionCount = 0;
-            Destroy(lineRenderer.gameObject);
-        }
-
-        gestureLinesRenderer.Clear();
+        particle.Clear();
+        particle.gameObject.SetActive(false);
     }
 
-    private void AddDrawnPoints()
-    {
-        points.Add(new Point(virtualKeyPosition.x, -virtualKeyPosition.y, strokeId));
-        currentGestureLineRenderer.positionCount = ++vertexCount;
-        currentGestureLineRenderer.SetPosition(vertexCount - 1, Camera.main.ScreenToWorldPoint(new Vector3(virtualKeyPosition.x, virtualKeyPosition.y, virtualKeyPosition.z)));
-        //Debug.Log(string.Format("Line Position: ({0}, {1}, {2})", currentGestureLineRenderer.GetPosition(vertexCount - 1).x, currentGestureLineRenderer.GetPosition(vertexCount - 1).y, currentGestureLineRenderer.GetPosition(vertexCount - 1).z));
-    }
-
-    private void InstantiateNewLine()
-    {
-        Transform tmpGesture = Instantiate(gestureOnScreenPrefab, transform.position, transform.rotation) as Transform;        
-        currentGestureLineRenderer = tmpGesture.GetComponent<LineRenderer>();
-        gestureLinesRenderer.Add(currentGestureLineRenderer);       
+    private Rect ConvertToScreenSize(RectTransform rt)
+    {        
+        Vector3[] _corners = new Vector3[4];
+        rt.GetWorldCorners(_corners);
+        Vector2 _scaledSize = new Vector2(rt.rect.size.x * rt.lossyScale.x, rt.rect.size.y * rt.lossyScale.y);
+        return new Rect(_corners[0], _scaledSize);
     }
 
     private void FinishDraw()
@@ -120,11 +83,25 @@ public class SkillDrawManager : MonoBehaviour
         Messenger<List<Point>>.Broadcast(GameEvent.SKILL_DRAW, points);
     }
 
-    private Rect ConvertToScreenSize(RectTransform rt)
-    {        
-        Vector3[] corners = new Vector3[4];
-        rt.GetWorldCorners(corners);
-        Vector2 scaledSize = new Vector2(rt.rect.size.x * rt.lossyScale.x, rt.rect.size.y * rt.lossyScale.y);
-        return new Rect(corners[0], scaledSize);
+    private Vector3 GetTouchPosition()
+    {
+        Vector3 _virtualKeyPosition = Vector3.zero;
+
+        if (platform == RuntimePlatform.Android)
+        {
+            if (Input.touchCount > 0)
+            {
+                _virtualKeyPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButton(0))
+            {
+                _virtualKeyPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+            }
+        }
+
+        return _virtualKeyPosition;
     }
 }
